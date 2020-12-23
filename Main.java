7bbf4e31a5bff44bb236bb35e56115a7450e18a1
Evan D <Evan_delasota@yahoo.com>
2020-12-23 03:26:08 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    String str = "ierdnA si eman yM iH";
+    char[] charInput = str.toCharArray();
+    String reversedInput = "";
+
+    System.out.println(str);
+
+    for (int i = charInput.length - 1; i >= 0; --i) {
+      reversedInput += charInput[i];
+    }
+
+    System.out.println(reversedInput);
+  }
+}
